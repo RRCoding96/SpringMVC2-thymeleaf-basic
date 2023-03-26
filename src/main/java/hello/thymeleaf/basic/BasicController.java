53ad548415325868823b405bdb7d2c606e9a01d0
Yoon-Uk.Kim <93039817+VicReznov@users.noreply.github.com>
2023-03-26 15:53:16 +0900

    feat: 유틸리티 객체, 날짜

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -64,6 +64,12 @@ public class BasicController {
         }
     }
 
+    @GetMapping("/date")
+    public String date(Model model) {
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
+    }
+
     @Data
     static class User {
         private String username;
